5899e7f7bdb5a9199bb3991e5bf813b60b2bba11
achmad fikri <fikriahmat29@gmail.com>
2024-10-27 19:17:09 +0700

    frontend

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order your favorite food here</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam illo quibusdam, reiciendis deleniti maxime in.
+                    Asperiores delectus ipsa culpa quo, necessitatibus blanditiis excepturi deleniti perferendis nobis vel architecto! Officiis, aliquam.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
